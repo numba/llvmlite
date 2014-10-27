449be69248cdb7165a837383236822d24f605d19
Antoine Pitrou <antoine@python.org>
2014-10-27 18:21:09 +0100

    Fix for 64-bit builds under Windows

--- ffi/build.py
+++ ffi/build.py
@@ -13,15 +13,20 @@ here_dir = os.path.abspath(os.path.dirname(__file__))
 build_dir = os.path.join(here_dir, 'build')
 target_dir = os.path.join(os.path.dirname(here_dir), 'llvmlite', 'binding')
 
+is_64bit = sys.maxsize >= 2**32
+
 
 def main_win32():
-    # NOTE: the LLVM build must have the same bitness as the Python runtime.
-    # I don't know if there's an easy way for us to check this.
+    # XXX: It would be nice if we could choose the generator's bitness
+    # (32/64) without hardcoding its full name...
     config = 'Release'
+    generator = 'Visual Studio 11 2012'
+    if is_64bit:
+        generator += ' Win64'
     if not os.path.isdir(build_dir):
         os.mkdir(build_dir)
     os.chdir(build_dir)
-    subprocess.check_call(['cmake', here_dir])
+    subprocess.check_call(['cmake', '-G', generator, here_dir])
     subprocess.check_call(['cmake', '--build', '.', '--config', config])
     shutil.copy(os.path.join(build_dir, config, 'llvmlite.dll'), target_dir)
 
