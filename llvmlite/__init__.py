1b04bdc4dd0a3f4aa533d1006d61f81764ffdf82
Antoine Pitrou <antoine@python.org>
2014-11-10 18:59:04 +0100

    Remove vendored enum34 (conda has it now)

--- llvmlite/__init__.py
+++ llvmlite/__init__.py
@@ -1,11 +1,3 @@
 from __future__ import absolute_import
 
 import sys
-
-# Inject enum module backport if necessary
-try:
-    import enum
-except ImportError:
-    from . import enum
-    sys.modules['enum'] = enum
-del enum
