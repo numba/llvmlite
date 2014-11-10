a9cd6a2e78d38367b4950ce0b62ea5a0be29bd8b
Antoine Pitrou <antoine@python.org>
2014-11-10 23:11:26 +0100

    Add Unix build script for llvmdev

---
+++ conda-recipes/llvmdev/build.sh
@@ -0,0 +1,13 @@
+./configure \
+    --enable-pic \
+    --enable-optimized \
+    --disable-docs \
+    --enable-targets=host \
+    --disable-terminfo \
+    --disable-libedit \
+    --prefix=$PREFIX \
+    --with-python=$SYS_PYTHON
+
+# libs-only, install-libs?
+make -j4 libs-only
+make install-libs
