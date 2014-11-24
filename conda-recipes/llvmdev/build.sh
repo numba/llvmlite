f117ef81a0fd250bf01e727c1bb6d183fb007522
Siu Kwan Lam <michael.lam.sk@gmail.com>
2014-11-24 11:28:09 -0600

    Fix build.sh for OSX - set deployment target to 10.7 with libc++ enabled

--- conda-recipes/llvmdev/build.sh
+++ conda-recipes/llvmdev/build.sh
@@ -1,12 +1,32 @@
-./configure \
-    --enable-pic \
-    --enable-optimized \
-    --disable-docs \
-    --enable-targets=host \
-    --disable-terminfo \
-    --disable-libedit \
-    --prefix=$PREFIX \
-    --with-python=$SYS_PYTHON
+if [ -z "$MACOSX_DEPLOYMENT_TARGET" ]; then
+    # Linux
+    ./configure \
+        --enable-pic \
+        --enable-optimized \
+        --disable-docs \
+        --enable-targets=host \
+        --disable-terminfo \
+        --disable-libedit \
+        --prefix=$PREFIX \
+        --with-python=$SYS_PYTHON
+
+else
+    # OSX needs 10.7 or above with libc++ enabled
+
+    export MACOSX_DEPLOYMENT_TARGET=10.7
+    ./configure \
+        --enable-pic \
+        --enable-optimized \
+        --disable-docs \
+        --enable-targets=host \
+        --disable-terminfo \
+        --disable-libedit \
+        --prefix=$PREFIX \
+        --with-python=$SYS_PYTHON \
+        --enable-libcpp=yes
+
+fi
+
 
 make -j4 libs-only
 make install-libs
