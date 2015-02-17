ab9ad227178d4609a6f8309c5a519b270fbb673b
Antoine Pitrou <antoine@python.org>
2015-02-17 15:50:44 +0100

    Fix compile error under Windows

--- ffi/initfini.cpp
+++ ffi/initfini.cpp
@@ -3,7 +3,11 @@
 #include "llvm-c/Target.h"
 
 #include "core.h"
+#ifdef _WIN32
+#include "llvm/Config/llvm-config.h"
+#else
 #include "llvm/Config/config.h"
+#endif
 
 extern "C" {
 
@@ -54,7 +58,10 @@ LLVMPY_GetVersionInfo()
     unsigned int verinfo = 0;
     verinfo += LLVM_VERSION_MAJOR << 16;
     verinfo += LLVM_VERSION_MINOR << 8;
+#ifdef LLVM_VERSION_PATCH
+    /* Not available under Windows... */
     verinfo += LLVM_VERSION_PATCH << 0;
+#endif
     return verinfo;
 }
 
