e262d8a3f0c9c580fe3d37059e003809ee506d74
Antoine Pitrou <antoine@python.org>
2015-03-12 20:12:38 +0100

    Revert bld.bat change

--- conda-recipes/llvmlite/bld.bat
+++ conda-recipes/llvmlite/bld.bat
@@ -5,5 +5,5 @@ set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
 @rem Ensure there are no build leftovers (CMake can complain)
 if exist ffi\build rmdir /S /Q ffi\build
 
-python setup.py install
+python -S setup.py install
 if errorlevel 1 exit 1
