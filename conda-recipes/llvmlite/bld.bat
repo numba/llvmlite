d6b897c88d7a214a87fa96227f604812d37a8a37
Antoine Pitrou <antoine@python.org>
2015-03-12 19:53:47 +0100

    Semi-random fix attempt for Jenkins

--- conda-recipes/llvmlite/bld.bat
+++ conda-recipes/llvmlite/bld.bat
@@ -5,5 +5,5 @@ set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
 @rem Ensure there are no build leftovers (CMake can complain)
 if exist ffi\build rmdir /S /Q ffi\build
 
-python -S setup.py install
+python setup.py install
 if errorlevel 1 exit 1
