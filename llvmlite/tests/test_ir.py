d97f4868d2be21e1bda1cde61941985985fdfb35
Antoine Pitrou <antoine@python.org>
2015-06-03 14:07:06 +0200

    Fix implicit termination in a nested if_then()

--- llvmlite/tests/test_ir.py
+++ llvmlite/tests/test_ir.py
@@ -755,6 +755,33 @@ class TestBuilderMisc(TestBase):
             one.endif.endif:
             """)
 
+    def test_if_then_nested(self):
+        # Implicit termination in a nested if/then
+        block = self.block(name='one')
+        builder = ir.IRBuilder(block)
+        z = ir.Constant(int1, 0)
+        a = builder.add(z, z, 'a')
+        with builder.if_then(a) as bbend:
+            b = builder.add(z, z, 'b')
+            with builder.if_then(b) as bbend:
+                c = builder.add(z, z, 'c')
+        builder.ret_void()
+        self.check_func_body(builder.function, """\
+            one:
+                %"a" = add i1 0, 0
+                br i1 %"a", label %"one.if", label %"one.endif"
+            one.if:
+                %"b" = add i1 0, 0
+                br i1 %"b", label %"one.if.if", label %"one.if.endif"
+            one.endif:
+                ret void
+            one.if.if:
+                %"c" = add i1 0, 0
+                br label %"one.if.endif"
+            one.if.endif:
+                br label %"one.endif"
+            """)
+
     def test_if_then_likely(self):
         def check(likely):
             block = self.block(name='one')
