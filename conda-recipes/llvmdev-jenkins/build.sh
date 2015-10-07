9e0c4a2bbb2bebb39a77ef6837667d55e9d99838
Antoine Pitrou <antoine@python.org>
2015-10-07 17:15:17 +0200

    Adjust llvm-dev recipes and travis.yml for LLVM 3.7

--- conda-recipes/llvmdev-jenkins/build.sh
+++ conda-recipes/llvmdev-jenkins/build.sh
@@ -1,20 +1,21 @@
+CMAKE_COMMON_VARIABLES=" -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=host \
+    -DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_UTILS=OFF \
+    -DLLVM_INCLUDE_DOCS=OFF -DLLVM_INCLUDE_EXAMPLES=OFF \
+    "
+
 if [ -z "$MACOSX_DEPLOYMENT_TARGET" ]; then
+    # Linux
     # Enable devtoolset-2, a newer gcc toolchain
     . /opt/rh/devtoolset-2/enable
     # Statically link the standard C/C++ library, because
     # we are building on an old centos5 machine.
     export CC=gcc
     export CXX=g++
-    # Linux
-    ./configure \
-        --enable-pic \
-        --enable-optimized \
-        --disable-docs \
-        --enable-targets=host \
-        --disable-terminfo \
-        --disable-libedit \
-        --prefix=$PREFIX \
-        --with-python=$SYS_PYTHON
+
+    mkdir build
+    cd build
+    cmake $CMAKE_COMMON_VARIABLES -DLLVM_USE_OPROFILE=ON ..
 
 else
     # OSX needs 10.7 or above with libc++ enabled
--- conda-recipes/llvmdev/build.sh
+++ conda-recipes/llvmdev/build.sh
@@ -1,14 +1,14 @@
+CMAKE_COMMON_VARIABLES=" -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=host \
+    -DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_UTILS=OFF \
+    -DLLVM_INCLUDE_DOCS=OFF -DLLVM_INCLUDE_EXAMPLES=OFF \
+    "
+
 if [ -z "$MACOSX_DEPLOYMENT_TARGET" ]; then
     # Linux
-    ./configure \
-        --enable-pic \
-        --enable-optimized \
-        --disable-docs \
-        --enable-targets=host \
-        --disable-terminfo \
-        --disable-libedit \
-        --prefix=$PREFIX \
-        --with-python=$SYS_PYTHON
+    mkdir build
+    cd build
+    cmake $CMAKE_COMMON_VARIABLES -DLLVM_USE_OPROFILE=ON ..
 
 else
     # OSX needs 10.7 or above with libc++ enabled
