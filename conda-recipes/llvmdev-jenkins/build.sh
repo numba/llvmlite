e45133721ae4b1fde3679fd2ea4f4c0993375db8
Antoine Pitrou <antoine@python.org>
2016-01-25 14:41:54 +0100

    Try to fix OS X builds

--- conda-recipes/llvmdev-jenkins/build.sh
+++ conda-recipes/llvmdev-jenkins/build.sh
@@ -16,24 +16,23 @@ fi
 if [ -n "$MACOSX_DEPLOYMENT_TARGET" ]; then
     # OSX needs 10.7 or above with libc++ enabled
     export MACOSX_DEPLOYMENT_TARGET=10.7
-    ./configure \
-        --enable-pic \
-        --enable-optimized \
-        --disable-docs \
-        --enable-targets=host \
-        --disable-terminfo \
-        --disable-libedit \
-        --prefix=$PREFIX \
-        --with-python=$SYS_PYTHON \
-        --enable-libcpp=yes
-
-else
-    # Use CMake-based build procedure
-    mkdir build
-    cd build
-    cmake $CMAKE_COMMON_VARIABLES -DLLVM_USE_OPROFILE=ON ..
-
 fi
+
+#     ./configure \
+#         --enable-pic \
+#         --enable-optimized \
+#         --disable-docs \
+#         --enable-targets=host \
+#         --disable-terminfo \
+#         --disable-libedit \
+#         --prefix=$PREFIX \
+#         --with-python=$SYS_PYTHON \
+#         --enable-libcpp=yes
+
+# Use CMake-based build procedure
+mkdir build
+cd build
+cmake $CMAKE_COMMON_VARIABLES -DLLVM_USE_OPROFILE=ON ..
 
 make -j4
 make install
