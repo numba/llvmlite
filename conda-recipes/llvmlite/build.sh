3d99af0e984eb755e563e8168c1b120e4bdfd65e
Antoine Pitrou <antoine@python.org>
2016-01-27 17:04:19 +0100

    Default to MACOSX_DEPLOYMENT_TARGET=10.9 in llvmlite recipes as well

--- conda-recipes/llvmlite/build.sh
+++ conda-recipes/llvmlite/build.sh
@@ -3,18 +3,14 @@
 toolset=/opt/rh/devtoolset-2
 
 if [ -d $toolset ]; then
-    # Use modified script to enable the toolset, as the original script
-    # assumes Python 2
-    . ./conda-recipes/llvmlite-jenkins/enable_devtoolset.sh
+    . /opt/rh/devtoolset-2/enable
+    export CC=gcc
+    export CXX=g++
 fi
 
-# If there is a system LLVM install of 3.5 or higher, prefer it over llvmdev
-# as it is synchronized with the system's libstdc++.
-llvm_config_candidate=/usr/bin/llvm-config
-
-if [ -x ${llvm_config_candidate} \
-     -a "$(${llvm_config_candidate} --version)" \> "3.5" ]
-then export LLVM_CONFIG=${llvm_config_candidate}
+if [ -n "$MACOSX_DEPLOYMENT_TARGET" ]; then
+    # OSX needs 10.7 or above with libc++ enabled
+    export MACOSX_DEPLOYMENT_TARGET=10.9
 fi
 
 export PYTHONNOUSERSITE=1
