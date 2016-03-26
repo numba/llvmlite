535b516d5bcff301a0e13aa7d712dc206480f3a7
whitequark <whitequark@whitequark.org>
2016-03-26 12:51:12 +0000

    Expose functions to create individual passes.

--- ffi/passmanagers.cpp
+++ ffi/passmanagers.cpp
@@ -1,5 +1,11 @@
 #include "core.h"
+#include "llvm-c/Transforms/Scalar.h"
+#include "llvm-c/Transforms/IPO.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/IPO.h"
 
+using namespace llvm;
 
 extern "C" {
 
@@ -46,6 +52,102 @@ API_EXPORT(int)
 LLVMPY_FinalizeFunctionPassManager(LLVMPassManagerRef FPM)
 {
     return LLVMFinalizeFunctionPassManager(FPM);
+}
+
+API_EXPORT(void)
+LLVMPY_AddConstantMergePass(LLVMPassManagerRef PM)
+{
+    LLVMAddConstantMergePass(PM);
+}
+
+API_EXPORT(void)
+LLVMPY_AddDeadArgEliminationPass(LLVMPassManagerRef PM)
+{
+    LLVMAddDeadArgEliminationPass(PM);
+}
+
+API_EXPORT(void)
+LLVMPY_AddFunctionAttrsPass(LLVMPassManagerRef PM)
+{
+    LLVMAddFunctionAttrsPass(PM);
+}
+
+API_EXPORT(void)
+LLVMPY_AddFunctionInliningPass(LLVMPassManagerRef PM, int Threshold)
+{
+    unwrap(PM)->add(createFunctionInliningPass(Threshold));
+}
+
+API_EXPORT(void)
+LLVMPY_AddGlobalOptimizerPass(LLVMPassManagerRef PM)
+{
+    LLVMAddGlobalOptimizerPass(PM);
+}
+
+API_EXPORT(void)
+LLVMPY_AddGlobalDCEPass(LLVMPassManagerRef PM)
+{
+    LLVMAddGlobalDCEPass(PM);
+}
+
+API_EXPORT(void)
+LLVMPY_AddIPSCCPPass(LLVMPassManagerRef PM)
+{
+    LLVMAddIPSCCPPass(PM);
+}
+
+API_EXPORT(void)
+LLVMPY_AddDeadCodeEliminationPass(LLVMPassManagerRef PM)
+{
+    unwrap(PM)->add(createDeadCodeEliminationPass());
+}
+
+API_EXPORT(void)
+LLVMPY_AddCFGSimplificationPass(LLVMPassManagerRef PM)
+{
+    LLVMAddCFGSimplificationPass(PM);
+}
+
+API_EXPORT(void)
+LLVMPY_AddGVNPass(LLVMPassManagerRef PM)
+{
+    LLVMAddGVNPass(PM);
+}
+
+API_EXPORT(void)
+LLVMPY_AddInstructionCombiningPass(LLVMPassManagerRef PM)
+{
+    LLVMAddInstructionCombiningPass(PM);
+}
+
+API_EXPORT(void)
+LLVMPY_AddLICMPass(LLVMPassManagerRef PM)
+{
+    LLVMAddLICMPass(PM);
+}
+
+API_EXPORT(void)
+LLVMPY_AddSCCPPass(LLVMPassManagerRef PM)
+{
+    LLVMAddSCCPPass(PM);
+}
+
+API_EXPORT(void)
+LLVMPY_AddSROAPass(LLVMPassManagerRef PM)
+{
+    unwrap(PM)->add(createSROAPass());
+}
+
+API_EXPORT(void)
+LLVMPY_AddTypeBasedAliasAnalysisPass(LLVMPassManagerRef PM)
+{
+    LLVMAddTypeBasedAliasAnalysisPass(PM);
+}
+
+API_EXPORT(void)
+LLVMPY_AddBasicAliasAnalysisPass(LLVMPassManagerRef PM)
+{
+    LLVMAddBasicAliasAnalysisPass(PM);
 }
 
 } // end extern "C"
