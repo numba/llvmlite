cd03f338e09064b3662716b080d5225be6b3a898
Stan Seibert <stan.seibert@continuum.io>
2016-06-25 20:50:37 +0000

    fix whitespace

--- llvmlite/tests/test_binding.py
+++ llvmlite/tests/test_binding.py
@@ -20,12 +20,12 @@ from . import TestCase
 
 # arvm7l needs extra ABI symbols to link successfully
 if platform.machine() == 'armv7l':
-    libgcc_s = ctypes.cdll.LoadLibrary('libgcc_s.so.1')    
+    libgcc_s = ctypes.cdll.LoadLibrary('libgcc_s.so.1')
     llvm.add_symbol('__aeabi_unwind_cpp_pr0',
                     ctypes.cast(libgcc_s.__aeabi_unwind_cpp_pr0,
                                 ctypes.c_void_p).value)
 
-    
+
 def no_de_locale():
     cur = locale.setlocale(locale.LC_ALL)
     try:
