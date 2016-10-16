b23a2805b45776ab233dd3e79f01e3ffea0fa795
whitequark <whitequark@whitequark.org>
2016-10-16 16:38:50 +0000

    Allow specifying alignment for alloca instructions.

--- llvmlite/ir/instructions.py
+++ llvmlite/ir/instructions.py
@@ -413,12 +413,15 @@ class AllocaInstr(Instruction):
         operands = [count] if count else ()
         super(AllocaInstr, self).__init__(parent, typ.as_pointer(), "alloca",
                                           operands, name)
+        self.align = None
 
     def descr(self, buf):
         buf.append("{0} {1}".format(self.opname, self.type.pointee))
         if self.operands:
             op, = self.operands
             buf.append(", {0} {1}".format(op.type, op.get_reference()))
+        if self.align is not None:
+            buf.append(", align {0}".format(self.align))
         if self.metadata:
             buf.append(self._stringify_metadata(leading_comma=True))
 
