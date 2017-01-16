2ab16bab024302c05979820938db47026e5bf005
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-01-16 15:02:02 -0600

    Drop oprofile support

--- conda-recipes/llvmdev/build.sh
+++ conda-recipes/llvmdev/build.sh
@@ -28,11 +28,7 @@ fi
 # Use CMake-based build procedure
 mkdir build
 cd build
-if [ "$platform" == 'linux' -a "$machine" != 'armv7l' ]; then
-    cmake $CMAKE_COMMON_VARIABLES -DLLVM_USE_OPROFILE=ON ..
-else
-    cmake $CMAKE_COMMON_VARIABLES ..
-fi
+cmake $CMAKE_COMMON_VARIABLES ..
 
 make -j8
 make install
