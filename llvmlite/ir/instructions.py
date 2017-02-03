79f45e1af3727858ddf4a689cd9e2ce9aef13595
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-02-03 17:01:58 -0600

    Fix flags in compare instructions

--- llvmlite/ir/instructions.py
+++ llvmlite/ir/instructions.py
@@ -309,19 +309,21 @@ class CompareInstr(Instruction):
         for flag in flags:
             if flag not in self.VALID_FLAG:
                 raise ValueError("invalid flag %r for %s" % (flag, self.OPNAME))
-        opname = " ".join([self.OPNAME] + flags)
+        opname = self.OPNAME
         super(CompareInstr, self).__init__(parent, types.IntType(1),
-                                           opname, [lhs, rhs], name=name)
+                                           opname, [lhs, rhs], flags=flags,
+                                           name=name)
         self.op = op
 
     def descr(self, buf):
-        buf.append("{0} {1} {2} {3}, {4} {5}\n".format(
-            self.opname,
-            self.op,
-            self.operands[0].type,
-            self.operands[0].get_reference(),
-            self.operands[1].get_reference(),
-            self._stringify_metadata(leading_comma=True),
+        buf.append("{opname}{flags} {op} {ty} {lhs}, {rhs} {meta}\n".format(
+            opname=self.opname,
+            flags=''.join(' ' + it for it in self.flags),
+            op=self.op,
+            ty=self.operands[0].type,
+            lhs=self.operands[0].get_reference(),
+            rhs=self.operands[1].get_reference(),
+            meta=self._stringify_metadata(leading_comma=True),
             ))
 
 
