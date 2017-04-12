48117f2fcb9539f69cfc70c6222a900f20025e15
Siu Kwan Lam <michael.lam.sk@gmail.com>
2017-04-12 11:14:22 -0500

    Fix wheel build Previously, it has to depend on user running `build_ext —inplace` before doing `bdist_wheel`.

--- setup.py
+++ setup.py
@@ -7,6 +7,11 @@ except ImportError:
     from distutils.command.install import install
 
 
+try:
+    from wheel.bdist_wheel import bdist_wheel
+except ImportError:
+    bdist_wheel = None
+
 from distutils.command.build import build
 from distutils.command.build_ext import build_ext
 from distutils.command.clean import clean
@@ -103,12 +108,24 @@ class LlvmliteClean(clean):
                         log.info("removing '%s'", fpath)
 
 
+if bdist_wheel:
+    class LLvmliteBDistWheel(bdist_wheel):
+        def run(self):
+            from llvmlite.utils import get_library_files
+            self.distribution.package_data = {
+                "llvmlite.binding": get_library_files(),
+            }
+            bdist_wheel.run(self)
+
+
 cmdclass.update({'build': LlvmliteBuild,
                  'build_ext': LlvmliteBuildExt,
                  'install': LlvmliteInstall,
                  'clean': LlvmliteClean,
                  })
 
+if bdist_wheel:
+    cmdclass.update({'bdist_wheel': LLvmliteBDistWheel})
 
 # A stub C-extension to make bdist_wheel build an arch dependent build
 ext_stub = Extension(name="llvmlite.binding._stub",
@@ -150,8 +167,4 @@ setup(name='llvmlite',
       license="BSD",
       cmdclass=cmdclass,
       ext_modules=[ext_stub] if not sys.platform.startswith('win32') else [],
-      package_data={
-          "llvmlite.binding": ["libllvmlite.dylib", "libllvmlite.so",
-                               "libllvmlite.dll"],
-        },
       )
