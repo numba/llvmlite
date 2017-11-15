44d3df2a5294a8fd1f5233a8df6a057627cd7428
Stuart Archibald <stuart.archibald@googlemail.com>
2017-11-15 12:00:05 +0000

    Fix for OSX
    Fixes issues with OSX and moves testing to test time.

--- conda-recipes/llvmlite/build.sh
+++ conda-recipes/llvmlite/build.sh
@@ -1,9 +1,30 @@
-# Note you may need to enable RH devtoolset-2 if building on an
-# old RH or CentOS system
+#!/bin/bash
+
+set -x
+
+if [[ $(uname) == Darwin ]]; then
+  ${SYS_PREFIX}/bin/conda create -y -p ${SRC_DIR}/bootstrap clangxx_osx-64
+  export PATH=${SRC_DIR}/bootstrap/bin:${PATH}
+  CONDA_PREFIX=${SRC_DIR}/bootstrap \
+    . ${SRC_DIR}/bootstrap/etc/conda/activate.d/*
+  export CONDA_BUILD_SYSROOT=${CONDA_BUILD_SYSROOT:-/opt/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk}
+  export CXXFLAGS=${CFLAGS}" -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
+  export CFLAGS=${CFLAGS}" -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
+  SYSROOT_DIR=${CONDA_BUILD_SYSROOT}
+  CFLAG_SYSROOT="--sysroot ${SYSROOT_DIR}"
+  # export LLVM_CONFIG explicitly as the one installed from llvmdev
+  # in the build root env, the one in the bootstrap location needs to be ignored.
+  export LLVM_CONFIG="${PREFIX}/bin/llvm-config"
+  ${LLVM_CONFIG} --version
+fi
 
 if [ -n "$MACOSX_DEPLOYMENT_TARGET" ]; then
     # OSX needs 10.7 or above with libc++ enabled
     export MACOSX_DEPLOYMENT_TARGET=10.9
+fi
+
+if [[ ${MACOSX_DEPLOYMENT_TARGET} == 10.9 ]]; then
+  DARWIN_TARGET=x86_64-apple-darwin13.4.0
 fi
 
 export PYTHONNOUSERSITE=1
@@ -12,4 +33,3 @@ export LLVMLITE_CXX_STATIC_LINK=1
 
 python setup.py build --force
 python setup.py install
-python runtests.py
