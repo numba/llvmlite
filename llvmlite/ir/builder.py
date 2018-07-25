9f5847c3e999de0af1983557433fa485c57a9d5d
Nick White <n.j.white@gmail.com>
2018-07-25 22:15:49 +0100

    Add FMA intrinsic support

--- llvmlite/ir/builder.py
+++ llvmlite/ir/builder.py
@@ -99,6 +99,26 @@ def _uniop_intrinsic_with_flag(opname):
     return wrap
 
 
+def _triop_intrinsic(opname):
+    def wrap(fn):
+        @functools.wraps(fn)
+        def wrapped(self, a, b, c,  name=''):
+            if a.type != b.type or b.type != c.type:
+                raise TypeError(
+                    "expected types to be the same, got %s, %s, %s" % (
+                        a.type,
+                        b.type,
+                        c.type))
+            elif not isinstance(a.type, (types.FloatType, types.DoubleType)):
+                raise TypeError("expected an floating point type, got %s" % a.type)
+            fn = self.module.declare_intrinsic(opname, [a.type, b.type, c.type])
+            return self.call(fn, [a, b, c], name)
+
+        return wrapped
+
+    return wrap
+
+
 def _castop(opname, cls=instructions.CastInstr):
     def wrap(fn):
         @functools.wraps(fn)
@@ -909,4 +929,10 @@ class IRBuilder(object):
     def cttz(self, cond, flag):
         """
         Counts the number of trailing zeros in a variable.
+        """
+
+    @_triop_intrinsic("llvm.fma")
+    def fma(self, a, b, c):
+        """
+        Perform the fused multiply-add operation.
         """
