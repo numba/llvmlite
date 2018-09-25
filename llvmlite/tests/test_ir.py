cd27255c169c4a39967d88b238c7381c70a69a6a
Dimitri Vorona <vorona@in.tum.de>
2018-09-25 17:37:30 +0200

    Fix gep method call on Constant objects

--- llvmlite/tests/test_ir.py
+++ llvmlite/tests/test_ir.py
@@ -1907,6 +1907,12 @@ class TestConstant(TestBase):
         self.assertEqual(str(c),
             'getelementptr ({float, i1}, {float, i1}* @"myconstant", i32 0, i32 1)')
         self.assertEqual(c.type, ir.PointerType(int1))
+        const = ir.Constant(tp.as_pointer(), None)
+        c2  = const.gep([ir.Constant(int32, 0)])
+        self.assertEqual(str(c2),
+            'getelementptr ({float, i1}, {float, i1}* null, i32 0)')
+        self.assertEqual(c.type, ir.PointerType(int1))
+
 
     def test_gep_addrspace_globalvar(self):
         m = self.module()
