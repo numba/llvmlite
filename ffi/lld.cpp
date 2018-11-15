5c04cfd288c43c3bda76517a5f5f28f060ea996d
Ondřej Čertík <ondrej@certik.us>
2018-11-14 17:43:04 -0700

    Add lld_main_help() for testing

--- ffi/lld.cpp
+++ ffi/lld.cpp
@@ -5,3 +5,8 @@ int lld_main(int Argc, const char **Argv) {
     std::vector<const char *> Args(Argv, Argv + Argc);
     return !lld::elf::link(Args, false);
 }
+
+int lld_main_help() {
+    std::vector<const char *> Args = {"ld.lld", "--help"};
+    return !lld::elf::link(Args, false);
+}
