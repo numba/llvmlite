5a2b50b6991c1461ce23017e685a66d06f7e1e19
Ondřej Čertík <ondrej@certik.us>
2018-11-14 17:39:18 -0700

    Rename the stub to lld_main

--- ffi/lld.cpp
+++ ffi/lld.cpp
@@ -1,7 +1,6 @@
 #include "lld/Common/Driver.h"
-#include <cstdlib>
 
-int main_test(int Argc, const char **Argv) {
+int lld_main(int Argc, const char **Argv) {
     //InitLLVM X(Argc, Argv);
     std::vector<const char *> Args(Argv, Argv + Argc);
     return !lld::elf::link(Args, false);
