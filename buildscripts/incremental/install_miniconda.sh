cf7a57aeea34e0ef28acb8d5e630de20301ef413
Dimitri Vorona <vorona@in.tum.de>
2018-12-06 09:29:06 +0100

    Put compiler override in build.py

--- buildscripts/incremental/install_miniconda.sh
+++ buildscripts/incremental/install_miniconda.sh
@@ -6,10 +6,6 @@ if [[ "$unamestr" == 'Linux' ]]; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 elif [[ "$unamestr" == 'Darwin' ]]; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
-  # use system clang in travis ci to avoid linking issues
-  # see https://github.com/bigartm/bigartm/issues/910
-  export CC=clang
-  export CXX=clang++
 else
   echo Error
 fi
