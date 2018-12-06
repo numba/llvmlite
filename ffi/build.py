9109a96635cd67899d7b15c54d61ed846a310e2b
Dimitri Vorona <vorona@in.tum.de>
2018-12-06 09:06:32 +0100

    Set the compiler in travis build script

--- ffi/build.py
+++ ffi/build.py
@@ -151,7 +151,7 @@ def main_posix(kind, library_ext):
             for lname in libs.split('-l') \
             if lname and 'LLVMCore' not in lname
             and 'LLVMSupport' not in lname]
-        # cxxflags.append(' '.join(excluded))
+        cxxflags.append(' '.join(excluded))
     # look for SVML
     include_dir = run_llvm_config(llvm_config, ['--includedir']).strip()
     svml_indicator = os.path.join(include_dir, 'llvm', 'IR', 'SVML.inc')
@@ -186,7 +186,7 @@ def build_passes():
                 generator = find_win32_generator()
                 try_cmake('..', '.', generator)
 
-            subprocess.check_call(['cmake', '--build', '.', '--config', 'Debug'])
+            subprocess.check_call(['cmake', '--build', '.', '--config', 'Release'])
             shutil.copy(os.path.join("hello", hello_pass_library), target_dir)
 
 
