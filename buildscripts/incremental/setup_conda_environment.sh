9c62c8034e9a304e385787bedb99e4936c382547
Dimitri Vorona <vorona@in.tum.de>
2018-12-07 09:36:56 +0100

    Switch to llvm 7

--- buildscripts/incremental/setup_conda_environment.sh
+++ buildscripts/incremental/setup_conda_environment.sh
@@ -27,7 +27,7 @@ source activate $CONDA_ENV
 set -v
 
 # Install llvmdev (separate channel, for now)
-$CONDA_INSTALL -c numba llvmdev="6.0*"
+$CONDA_INSTALL -c numba llvmdev="7.0*"
 $CONDA_INSTALL cmake
 
 # Install the compiler toolchain, for osx, bootstrapping needed
