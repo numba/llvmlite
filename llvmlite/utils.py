e1f2cd4c4c0eb17d28cf689fb53db79a763e5c17
Dimitri Vorona <vorona@in.tum.de>
2018-12-10 12:52:50 +0100

    Build a module on osx

--- llvmlite/utils.py
+++ llvmlite/utils.py
@@ -11,10 +11,7 @@ def get_library_name():
     Return the name of the llvmlite shared library file.
     """
     if os.name == 'posix':
-        if sys.platform == 'darwin':
-            return 'libllvmlite.dylib'
-        else:
-            return 'libllvmlite.so'
+        return 'libllvmlite.so'
     else:
         assert os.name == 'nt'
         return 'llvmlite.dll'
