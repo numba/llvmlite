75ad3ef4080f22121b9e25cb11e865c6fa670f38
Valentin Haenel <vhaenel@anaconda.com>
2020-04-21 10:22:43 +0200

    remove setup and install dependencies
    Since 3.4 is no longer a supported Python version, we can safely remove
    dependencies needed on that version.

--- setup.py
+++ setup.py
@@ -162,9 +162,6 @@ packages = ['llvmlite',
             'llvmlite.tests',
             ]
 
-install_requires = ['enum34; python_version < "3.4"']
-setup_requires = ['enum34; python_version < "3.4"']
-
 
 with open('README.rst') as f:
     long_description = f.read()
@@ -191,8 +188,6 @@ setup(name='llvmlite',
       url="http://llvmlite.pydata.org",
       download_url="https://github.com/numba/llvmlite",
       packages=packages,
-      install_requires=install_requires,
-      setup_requires=setup_requires,
       license="BSD",
       cmdclass=cmdclass,
       long_description=long_description,
