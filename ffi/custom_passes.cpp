364450fa95b8dbd85df853fb69f721ce21ef77d6
Siu Kwan Lam <michael.lam.sk@gmail.com>
2020-08-27 15:54:53 -0500

    Handle basic fanout case in refops pruning

--- ffi/custom_passes.cpp
+++ ffi/custom_passes.cpp
@@ -7,6 +7,9 @@
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Instructions.h"
 
+#include "llvm/ADT/SmallString.h"
+#include "llvm/ADT/SetVector.h"
+
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Analysis/Passes.h"
 #include "llvm/Analysis/RegionPass.h"
@@ -25,27 +28,13 @@
 #include "llvm/LinkAllPasses.h"
 
 #include <vector>
+#include <map>
 
 using namespace llvm;
 
 namespace llvm {
     void initializeRefPrunePassPass(PassRegistry &Registry);
 }
-/*
-struct Hello : public RegionPass {
-  static char ID;
-  Hello() : RegionPass(ID) {
-      initializeHelloPass(*PassRegistry::getPassRegistry());
-  }
-
-  bool runOnRegion(Region *region, RGPassManager &RGM) override {
-    errs() << "Hello: ";
-    errs().write_escaped(region->getNameStr()) << '\n';
-    return false;
-  }
-}; // end of struct Hello
-*/
-
 
 struct RefPrunePass : public FunctionPass {
   static char ID;
@@ -66,7 +55,7 @@ struct RefPrunePass : public FunctionPass {
     bool mutated = false;
 
     // Find all incref & decref
-    std::vector<CallInst*> incref_list, decref_list;
+    SmallVector<CallInst*, 20> incref_list, decref_list;
     for (BasicBlock &bb : F) {
         for (Instruction &ii : bb) {
             if (ii.getOpcode() == Instruction::Call) {
@@ -85,22 +74,10 @@ struct RefPrunePass : public FunctionPass {
     errs() << "Counts " << incref_list.size() << " " << decref_list.size() << "\n";
 
     // Drop refops on NULL pointers
-    for (CallInst*& refop: incref_list) {
-        if (!keepNonNullArg(refop)) {
-            refop->eraseFromParent();
-            mutated |= true;
-            refop = NULL;
-        }
-    }
-    for (CallInst*& refop: decref_list) {
-        if (!keepNonNullArg(refop)) {
-            refop->eraseFromParent();
-            mutated |= true;
-            refop = NULL;
-        }
-    }
+    mutated |= eraseNullFirstArgFromList(incref_list);
+    mutated |= eraseNullFirstArgFromList(decref_list);
 
-    // check pairs that are dominating and postdominating each other
+    // Check pairs that are dominating and postdominating each other
     for (CallInst* incref: incref_list) {
         if (incref == NULL) continue;
 
@@ -120,11 +97,115 @@ struct RefPrunePass : public FunctionPass {
                 decref->eraseFromParent();
                 decref = NULL;
                 mutated |= true;
-                continue;
             }
         }
     }
+
+    // Deal with fanout
+    // a single incref with multiple decrefs in outgoing edges
+    for (CallInst*& incref : incref_list) {
+        if (incref == NULL) continue;
+
+        SetVector<CallInst*> candidates;
+
+        Instruction* term = incref->getParent()->getTerminator();
+
+        bool balanced = true;
+        errs() << "======= AT\n" ;
+        incref->dump();
+        for (unsigned int i = 0; i < term->getNumSuccessors(); ++i) {
+            DomTreeNode* domchild = domtree.getNode(term->getSuccessor(i));
+
+            errs() << "==== check " << domchild->getBlock()->getName() << "\n";
+            if (!findDecrefDominatedByNode(domtree, postdomtree, domchild, incref,
+                                           candidates)) {
+                balanced = false;
+            }
+        }
+        if (balanced) {
+            errs() << "======balanced" << "\n";
+            for (CallInst* inst : candidates)  {
+                inst->dump();
+            }
+            errs() << "======" << "\n";
+            incref->eraseFromParent();
+            for (CallInst* inst : candidates)  {
+                inst->eraseFromParent();
+            }
+            incref = NULL;
+            mutated |= true;
+        }
+
+    }
     return mutated;
+  }
+
+  template <class T>
+  bool eraseNullFirstArgFromList(T& refops) {
+    bool mutated = false;
+    for (CallInst*& refop: refops) {
+        if (!isNonNullFirstArg(refop)) {
+            refop->eraseFromParent();
+            mutated |= true;
+            refop = NULL;
+        }
+    }
+    return mutated;
+  }
+
+  bool findDecrefDominatedByNode(DominatorTree &domtree,
+                               PostDominatorTree &postdomtree,
+                               DomTreeNode* dom_root,
+                               CallInst* incref,
+                               SetVector<CallInst*> &candidates,
+                               unsigned int depth = 0) {
+
+    bool found = false;
+    BasicBlock* bb_root = dom_root->getBlock();
+
+    errs() << "   -- findDecrefDominatedByNode: " << bb_root->getName() << "\n";
+
+    for (CallInst *decref : findRelatedDecrefs(dom_root->getBlock(), incref) ){
+        if ( domtree.dominates(incref, decref) ){
+            errs() << "   found\n";
+            decref->dump();
+
+            candidates.insert(decref);
+            found = true;
+            // stop on first result
+            break;
+        }
+    }
+
+    if (found) return true;
+
+    for (DomTreeNode* dom_child : dom_root->getChildren()) {
+        if (!findDecrefDominatedByNode(domtree, postdomtree, dom_child, incref, candidates)) {
+            return false;
+        }
+    }
+    return true;
+  }
+
+  /**
+   * Find related decrefs to incref inside a basicblock in order
+   */
+  std::vector<CallInst*> findRelatedDecrefs(BasicBlock* bb, CallInst* incref) {
+      std::vector<CallInst*> res;
+      for (Instruction &ii : *bb) {
+        if (ii.getOpcode() == Instruction::Call) {
+            CallInst *call_inst = dyn_cast<CallInst>(&ii);
+            Value *callee = call_inst->getCalledOperand();
+            if ( callee->getName() != "NRT_decref" ) {
+                continue;
+            }
+            if (incref->getArgOperand(0) != call_inst->getArgOperand(0)) {
+                continue;
+            }
+            res.push_back(call_inst);
+        }
+      }
+      return res;
   }
 
    void getAnalysisUsage(AnalysisUsage &Info) const override {
@@ -132,7 +213,7 @@ struct RefPrunePass : public FunctionPass {
        Info.addRequired<PostDominatorTreeWrapperPass>();
    }
 
-   bool keepNonNullArg(CallInst *call_inst){
+   bool isNonNullFirstArg(CallInst *call_inst){
        auto val = call_inst->getArgOperand(0);
        auto ptr = dyn_cast<ConstantPointerNull>(val);
        return ptr == NULL;
