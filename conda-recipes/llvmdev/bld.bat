c1d410856676cfb2b419ba7815fa2240c5202f46
Valentin Haenel <vhaenel@anaconda.com>
2020-12-16 13:17:11 +0100

    Revert "experimental: simplify script to use only Visual Studio 16 2019"
    This reverts commit 6594546243dd7c0fcc2e0686b2723090e7b9011b.

--- conda-recipes/llvmdev/bld.bat
+++ conda-recipes/llvmdev/bld.bat
@@ -9,11 +9,24 @@ REM allow setting the targets to build as an environment variable
 if "%LLVM_TARGETS_TO_BUILD%"=="" (
     set "LLVM_TARGETS_TO_BUILD=host;AMDGPU;NVPTX"
 )
-set CMAKE_GENERATOR="Visual Studio 16 2019"
-set CMAKE_GENERATOR_TOOLSET="v141"
+if "%ARCH%"=="32" (
+    set "ARCH_POSTFIX="
+) else (
+    set "ARCH_POSTFIX= Win64"
+)
+set "CMAKE_GENERATOR[0]=Visual Studio 14 2015%ARCH_POSTFIX%"
+set "CMAKE_GENERATOR[1]=Visual Studio 15 2017%ARCH_POSTFIX%"
+set "CMAKE_GENERATOR[2]=Visual Studio 16 2019"
+set MAX_INDEX_CMAKE_GENERATOR=2
+set CMAKE_GENERATOR_TOOLSET=v141
 
+REM the platform toolset host arch is set to x64 so as to use the 64bit linker,
+REM the 32bit linker heap is too small for llvm8 so it tries and falls over to
+REM the 64bit linker anyway
+set PreferredToolArchitecture=x64
 
 REM Reduce build times and package size by removing unused stuff
+REM BENCHMARKS (new for llvm8) don't build under Visual Studio 14 2015
 set CMAKE_CUSTOM=-DLLVM_TARGETS_TO_BUILD="%LLVM_TARGETS_TO_BUILD%" ^
     -DLLVM_INCLUDE_TESTS=OFF -DLLVM_INCLUDE_UTILS=ON -DLLVM_INCLUDE_DOCS=OFF ^
     -DLLVM_INCLUDE_EXAMPLES=OFF -DLLVM_ENABLE_ASSERTIONS=ON ^
@@ -22,11 +35,20 @@ set CMAKE_CUSTOM=-DLLVM_TARGETS_TO_BUILD="%LLVM_TARGETS_TO_BUILD%" ^
     -DLLVM_ENABLE_DIA_SDK=OFF ^
     -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=WebAssembly
 
-cmake -G "%CMAKE_GENERATOR%" -T "%CMAKE_GENERATOR_TOOLSET%" ^
-    -DCMAKE_BUILD_TYPE="%BUILD_CONFIG%" -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" %CMAKE_CUSTOM% "%SRC_DIR%"
+REM try all compatible visual studio toolsets to find one that is installed
+setlocal enabledelayedexpansion
+for /l %%n in (0,1,%MAX_INDEX_CMAKE_GENERATOR%) do (
+    cmake -G "!CMAKE_GENERATOR[%%n]!" -T "%CMAKE_GENERATOR_TOOLSET%" ^
+        -DCMAKE_BUILD_TYPE="%BUILD_CONFIG%" -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+        -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" %CMAKE_CUSTOM% "%SRC_DIR%"
+    if not errorlevel 1 goto configuration_successful
+    del CMakeCache.txt
+)
 
+REM no compatible visual studio toolset was found
 
+:configuration_successful
+endlocal
 REM === Build step ===
 cmake --build . --config "%BUILD_CONFIG%"
 if errorlevel 1 exit 1
