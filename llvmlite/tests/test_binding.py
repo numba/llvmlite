727ad06e4b8c558d06d421c05e8f237a53665d22
Valentin Haenel <vhaenel@anaconda.com>
2021-04-19 15:27:00 +0200

    fix LLVM version in test too
    As title

--- llvmlite/tests/test_binding.py
+++ llvmlite/tests/test_binding.py
@@ -398,7 +398,7 @@ class TestMisc(BaseTest):
     def test_version(self):
         major, minor, patch = llvm.llvm_version_info
         # one of these can be valid
-        valid = [(10, 0), (9, 0)]
+        valid = [(11, 1), (9, 0)]
         self.assertIn((major, minor), valid)
         self.assertIn(patch, range(10))
 
