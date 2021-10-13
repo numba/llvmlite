8cc3c39515ac03dc77d3b8ea4370513cec0e4308
Valentin Haenel <vhaenel@anaconda.com>
2021-10-13 11:02:51 +0200

    buffer all errors, and echo them at the end
    This will avoid printing errors unnecessarily.

--- llvmlite/binding/ffi.py
+++ llvmlite/binding/ffi.py
@@ -180,16 +180,19 @@ else:
 
 
 # Try to load from all of the different paths
+errors = []
 for _lib_path in _lib_paths:
     try:
         lib = ctypes.CDLL(_lib_path)
     except OSError as e:
-        print(e)
+        errors.append(e)
         continue
     else:
         break
 else:
-    raise OSError("Could not load shared object file: {}".format(_lib_name))
+    msg = ("Could not load shared object file: {}\n".format(_lib_name) +
+           "Errors were: {}".format(errors))
+    raise OSError(msg)
 
 
 lib = _lib_wrapper(lib)
