ecfe984ce510575587506ac8189cace3ebf3f072
Michael Collison <collison956@gmail.com>
2023-04-20 23:56:11 -0400

    Remove final use of .c test files

--- setup.py
+++ setup.py
@@ -153,7 +153,6 @@ if bdist_wheel:
             build_library_files(self.dry_run)
             self.distribution.package_data.update({
                 "llvmlite.binding": get_library_files(),
-                "llvmlite.tests": ["*.c"]
             })
             # Run wheel build command
             bdist_wheel.run(self)
