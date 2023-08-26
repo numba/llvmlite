f93933ae72fe4c0d4d50c421650592c35522c96f
oliverhu <khu@apache.org>
2023-08-26 14:20:47 -0700

    test ci

--- buildscripts/incremental/setup_conda_environment.sh
+++ buildscripts/incremental/setup_conda_environment.sh
@@ -27,7 +27,7 @@ source activate $CONDA_ENV
 set -v
 
 # TEMPORARY
-$CONDA_INSTALL gmarkall::llvmdev
+$CONDA_INSTALL ohu::llvmdev
 # Install llvmdev (separate channel, for now)
 # $CONDA_INSTALL -c numba/label/dev llvmdev="14.*"
 
