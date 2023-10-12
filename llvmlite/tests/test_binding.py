6fc14666270f9d033d4fb6400c34b7a581bf8e84
Graham Markall <gmarkall@nvidia.com>
2023-10-12 12:08:12 +0100

    Skip OrcJIT tests on non-x86 platforms
    Since OrcJIT is still experimental, and there seem to be some issues on
    platforms we don't regularly test on (e.g. Issue #1000), it seems
    prudent to simply skip the OrcJIT tests on non-x86 platforms.

--- llvmlite/tests/test_binding.py
+++ llvmlite/tests/test_binding.py
@@ -1222,6 +1222,11 @@ class TestMCJit(BaseTest, JITWithTMTestMixin):
         return llvm.create_mcjit_compiler(mod, target_machine)
 
 
+# There are some memory corruption issues with OrcJIT on AArch64 - see Issue
+# #1000. Since OrcJIT is experimental, and we don't test regularly during
+# llvmlite development on non-x86 platforms, it seems safest to skip these
+# tests on non-x86 platforms.
+@unittest.skipUnless(platform.machine().startswith("x86"), "x86 only")
 class TestOrcLLJIT(BaseTest):
 
     def jit(self, asm=asm_sum, func_name="sum", target_machine=None,
