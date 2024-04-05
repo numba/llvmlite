20b250c3ccbd3ceb9ee18a11067393643d603755
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2024-04-05 08:48:45 -0500

    Filter out osx test:
    LLVM ERROR: Tried to execute an unknown external function: roundevenf

--- conda-recipes/llvmdev_llvm15/build.sh
+++ conda-recipes/llvmdev_llvm15/build.sh
@@ -102,7 +102,8 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
   if [[ "$target_platform" == osx-* ]]; then
     # This failure seems like something to do with the output format of ls -lu
     # and looks harmless
-    export LIT_FILTER_OUT='tools/llvm-objcopy/ELF/strip-preserve-atime.test'
+    export LIT_FILTER_OUT='tools/llvm-objcopy/ELF/strip-preserve-atime.test|'\
+                          'ExecutionEngine/Interpreter/intrinsics.ll'
   fi
 
   # if [[ "${target_platform}" != "linux-ppc64le" && "${target_platform}" !~ ^osx  ]]; then
