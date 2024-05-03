4de4afb659687c394bdfc230b36a3c367370951b
modimo <modimo@fb.com>
2024-05-02 20:18:03 -0700

    Add ffi_types.h

---
+++ ffi/ffi_types.h
@@ -0,0 +1,28 @@
+
+#pragma once
+
+#include "core.h"
+
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/IR/PassTimingInfo.h"
+
+typedef llvm::PassBuilder *LLVMPassBuilder;
+
+typedef llvm::FunctionPassManager *LLVMFunctionPassManager;
+
+typedef llvm::ModulePassManager *LLVMModulePassManager;
+
+typedef llvm::FunctionAnalysisManager *LLVMFunctionAnalysisManager;
+
+typedef llvm::ModuleAnalysisManager *LLVMModuleAnalysisManager;
+
+typedef llvm::CGSCCAnalysisManager *LLVMCGSCCAnalysisManager;
+
+typedef llvm::LoopAnalysisManager *LLVMLoopAnalysisManager;
+
+typedef llvm::PassInstrumentationCallbacks *LLVMPassInstrumentationCallbacks;
+
+typedef llvm::TimePassesHandler *LLVMTimePassesHandler;
+
+typedef llvm::OptimizationLevel const *LLVMOptimizationLevel;
