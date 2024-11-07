1662dc6b825dd0d4c80f05499e49a8d103abd9c2
swap357 <swapnilpatel357@gmail.com>
2024-11-07 13:14:59 -0800

    upgrade to macos-13

--- buildscripts/incremental/build.sh
+++ buildscripts/incremental/build.sh
@@ -9,14 +9,31 @@ if [[ $(uname) == Darwin ]]; then
   # export LLVM_CONFIG explicitly as the one installed from llvmdev
   # in the build root env, the one in the bootstrap location needs to be ignored.
   export LLVM_CONFIG=$(ls $(which llvm-config))
-  # now bootstrap the toolchain for building
-  conda create -y -p ${PWD}/bootstrap clangxx_osx-64
+  
+  # Determine architecture
+  ARCH=$(uname -m)
+  if [[ "$ARCH" == "arm64" ]]; then
+    CONDA_SUBDIR=osx-arm64
+    # Bootstrap with ARM64 compiler
+    conda create -y -p ${PWD}/bootstrap clangxx_osx-arm64
+  else
+    CONDA_SUBDIR=osx-64
+    # Bootstrap with x86_64 compiler
+    conda create -y -p ${PWD}/bootstrap clangxx_osx-64
+  fi
+  
   SRC_DIR=${PWD}
   export PATH=${SRC_DIR}/bootstrap/bin:${PATH}
   CONDA_PREFIX=${SRC_DIR}/bootstrap \
     . ${SRC_DIR}/bootstrap/etc/conda/activate.d/*
   SDKPATH=$(xcrun --show-sdk-path)
   export CONDA_BUILD_SYSROOT=${CONDA_BUILD_SYSROOT:-${SDKPATH}}
+  
+  # Set minimum deployment target
+  if [ -z "$MACOSX_DEPLOYMENT_TARGET" ]; then
+    export MACOSX_DEPLOYMENT_TARGET=11.0
+  fi
+  
   export CXXFLAGS=${CFLAGS}" -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
   export CFLAGS=${CFLAGS}" -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
   SYSROOT_DIR=${CONDA_BUILD_SYSROOT}
