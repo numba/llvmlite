332e14347369a5ad511b0c02e968dd0d2fb9f6ca
Benjamin Austin Jr <spidertyler1122@gmail.com>
2025-01-18 19:00:44 -0500

    try libstdcxx-ng=11

--- buildscripts/incremental/setup_conda_environment.sh
+++ buildscripts/incremental/setup_conda_environment.sh
@@ -30,25 +30,21 @@ set -v
 if [ "$LLVM" == "16" ]; then
     # also install lld, not required when using the numba channel because the numba
     #  channel includes lld.
-    if [[ $(uname) == Linux ]]; then
-        conda update libstdcxx-ng
-    else
-        $CONDA_INSTALL -c conda-forge libcxx
-    fi
     $CONDA_INSTALL -c conda-forge llvmdev="16" lld="16"
 else
     # Install the compiler toolchain, for osx, bootstrapping needed
     # which happens in build.sh
-    if [[ $(uname) == Linux ]]; then
-        # $CONDA_INSTALL gcc_linux-64 gxx_linux-64
-        $CONDA_INSTALL -c conda-forge libstdcxx-ng=12
-    else
-        $CONDA_INSTALL -c conda-forge libcxx
-    fi
     $CONDA_INSTALL -c numba llvmdev="15.*"
 fi
 
 $CONDA_INSTALL -c numba libxml2
+
+if [[ $(uname) == Linux ]]; then
+    # $CONDA_INSTALL gcc_linux-64 gxx_linux-64
+    $CONDA_INSTALL -c conda-forge libstdcxx-ng=11
+else
+    $CONDA_INSTALL -c conda-forge libcxx
+fi
 
 # Install dependencies for code coverage (codecov.io)
 if [ "$RUN_COVERAGE" == "yes" ]; then $PIP_INSTALL codecov coveralls; fi
