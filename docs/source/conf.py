16e8cfdb9ed17cbdb602cd3ce3786c08c9bb34f2
Benjamin Austin Jr <spidertyler1122@gmail.com>
2025-01-20 18:21:38 -0500

    remove deprecated get_html_theme_path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -123,7 +123,7 @@ todo_include_todos = False
 # a list of builtin themes.
 
 html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = []
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
