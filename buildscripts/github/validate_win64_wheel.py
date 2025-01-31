406af5fc442d6d058e9dfaa54dd9d0c8a769ac01
Swapnil Patel <swapnilpatel357@gmail.com>
2025-02-01 03:23:20 +0530

    llvmlite win whl builder gha (#15)
    * adding .github/workflows/llvmdev_win-64_conda_builder.yml
    
    As title
    
    * increase retention time to 7 days
    
    As title
    
    * add win-64 wheel builder GHA workflow
    
    * refactor win-64 wheel builder workflow
    
    ---------
    
    Co-authored-by: esc <esc@users.noreply.github.com>

---
+++ buildscripts/github/validate_win64_wheel.py
@@ -0,0 +1,33 @@
+import lief
+import pathlib
+
+for path in pathlib.Path('.').rglob("**/*.dll"):
+    print("path", path)
+    dll = lief.PE.parse(str(path))
+
+    if hasattr(dll, "delay_imports"):
+        delay_imports = {x.name for x in dll.delay_imports}
+        print("Delay imports:", delay_imports)
+        expected_delay_imports = {'SHELL32.dll', 'ole32.dll'}
+        assert delay_imports == expected_delay_imports, f"Unexpected delay imports: {delay_imports}"
+
+    imports = {x.name for x in dll.imports}
+    print("Regular imports:", imports)
+    expected_imports = {
+        'ADVAPI32.dll',
+        'KERNEL32.dll',
+        'MSVCP140.dll',
+        'VCRUNTIME140.dll',
+        'VCRUNTIME140_1.dll',
+        'api-ms-win-crt-convert-l1-1-0.dll',
+        'api-ms-win-crt-environment-l1-1-0.dll',
+        'api-ms-win-crt-heap-l1-1-0.dll',
+        'api-ms-win-crt-locale-l1-1-0.dll',
+        'api-ms-win-crt-math-l1-1-0.dll',
+        'api-ms-win-crt-runtime-l1-1-0.dll',
+        'api-ms-win-crt-stdio-l1-1-0.dll',
+        'api-ms-win-crt-string-l1-1-0.dll',
+        'api-ms-win-crt-time-l1-1-0.dll',
+        'api-ms-win-crt-utility-l1-1-0.dll'
+    }
+    assert imports == expected_imports, f"Unexpected imports: {imports}"
