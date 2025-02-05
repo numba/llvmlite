4425700dca13f06b868c2789142c2c1f5e687609
Daniel Bast <2790401+dbast@users.noreply.github.com>
2025-02-05 15:01:01 +0100

    Reduce default matrix, change label filter, match up/download naming

--- buildscripts/github/llvmdev_evaluate.py
+++ buildscripts/github/llvmdev_evaluate.py
@@ -23,18 +23,8 @@ default_include = [
         "recipe": "llvmdev"
     },
     {
-        "runner": runner_mapping["linux-aarch64"],
-        "platform": "linux-aarch64",
-        "recipe": "llvmdev",
-    },
-    {
-        "runner": runner_mapping["osx-arm64"],
-        "platform": "osx-arm64",
-        "recipe": "llvmdev",
-    },
-    {
-        "runner": runner_mapping["osx-arm64"],
-        "platform": "osx-arm64",
+        "runner": runner_mapping["linux-64"],
+        "platform": "linux-64",
         "recipe": "llvmdev_manylinux",
     },
     {   "runner": runner_mapping["win-64"],
@@ -52,7 +42,7 @@ print(f"Deciding what to do based on event: '{event}', label: '{label}', inputs:
 if event == "pull_request":
     print("pull_request detected")
     include = default_include
-elif event == "label" and label == "build":
+elif event == "label" and label == "build_on_gha":
     print("build label detected")
     include = default_include
 elif event == "workflow_dispatch":
