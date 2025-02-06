5b5e45145d17f3945d5074aa2bc7aa518f9cde1c
esc <esc@users.noreply.github.com>
2025-02-06 17:21:05 +0100

    fix typo
    As title

--- llvmlite/tests/test_binding.py
+++ llvmlite/tests/test_binding.py
@@ -728,7 +728,7 @@ class TestDependencies(BaseTest):
             if not dep.startswith('ld-linux-') and dep not in allowed:
                 fails.append(dep)
         if len(fails) == 1:
-            self.fail("unexpected dependeny %r in %r" % (fails[0], deps))
+            self.fail("unexpected dependency %r in %r" % (fails[0], deps))
         elif len(fails) > 1:
             self.fail("unexpected dependencies %r in %r" % (fails, deps))
         else:
