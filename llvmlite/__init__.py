b2619197ec54b9b9f8b1ec3c4323e03a58929fcf
Ricardo Jesus <rjj@nvidia.com>
2025-02-07 16:07:14 +0000

    Add lazy opaque pointers mode

--- llvmlite/__init__.py
+++ llvmlite/__init__.py
@@ -3,8 +3,12 @@ __version__ = get_versions()['version']
 del get_versions
 
 # FIXME: Remove me once typed pointers are no longer supported.
-def _opaque_pointers_enabled():
+# Let's enable opaque pointers unconditionally.
+opaque_pointers_enabled = True
+# Note: We should probably default to lazy opaque pointers disabled, but I want
+# us to be able to test with Numba directly.
+def _lazy_opaque_pointers_enabled():
   import os
-  return os.environ.get('LLVMLITE_ENABLE_OPAQUE_POINTERS', '0') == '1'
-opaque_pointers_enabled = _opaque_pointers_enabled()
-del _opaque_pointers_enabled
+  return os.environ.get('LLVMLITE_ENABLE_LAZY_OPAQUE_POINTERS', '1') == '1'
+lazy_opaque_pointers_enabled = _lazy_opaque_pointers_enabled()
+del _lazy_opaque_pointers_enabled
