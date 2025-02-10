b33d8b82ff07da1850eb12c212c7495df3cb0bea
esc <esc@users.noreply.github.com>
2025-02-10 18:06:09 +0100

    flake8
    As title

--- buildscripts/github/validate_win64_wheel.py
+++ buildscripts/github/validate_win64_wheel.py
@@ -32,6 +32,7 @@ for path in pathlib.Path(".").rglob("**/*.dll"):
         "api-ms-win-crt-time-l1-1-0.dll",
         "api-ms-win-crt-utility-l1-1-0.dll",
     }
-    assert (
-        imports == expected_imports
-    ), f"Unexpected imports: {imports - expected_imports}\nMissing imports: {expected_imports - imports}"
+    assert imports == expected_imports, (
+        f"Unexpected imports: {imports - expected_imports}\n"
+        f"Missing imports: {expected_imports - imports}"
+    )
