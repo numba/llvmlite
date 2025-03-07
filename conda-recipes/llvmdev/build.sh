34eaea0d53377990fb25d7028a1bc01bd0462a8e
swap357 <swapnilpatel357@gmail.com>
2025-03-07 09:19:19 -0800

    move sdk checksum file to correct location

--- conda-recipes/llvmdev/build.sh
+++ conda-recipes/llvmdev/build.sh
@@ -8,7 +8,7 @@ set -x
 if [[ $(uname) == "Darwin" ]]; then
     # The following is suggested in https://docs.conda.io/projects/conda-build/en/latest/resources/compiler-tools.html?highlight=SDK#macos-sdk
     wget -q https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX10.10.sdk.tar.xz
-    shasum -c ./buildscripts/incremental/MacOSX10.10.sdk.checksum
+    shasum -c ./conda-recipes/llvmdev/MacOSX10.10.sdk.checksum
     tar -xf ./MacOSX10.10.sdk.tar.xz
     SDK_PATH=`pwd`/MacOSX10.10.sdk
     export SDKROOT=$SDK_PATH
