5bb8111fed0de8ddb079358b3835344a01a7df22
swap357 <swapnilpatel357@gmail.com>
2025-06-11 19:47:21 -0700

    add VS2022 environment setup to bld.bat for llvmdev and llvmdev_for_wheel recipes

--- conda-recipes/llvmdev/bld.bat
+++ conda-recipes/llvmdev/bld.bat
@@ -1,6 +1,18 @@
 REM base on https://github.com/AnacondaRecipes/llvmdev-feedstock/blob/master/recipe/bld.bat
 echo on
 
+REM First, setup the VS2022 environment
+for /F "usebackq tokens=*" %%i in (`vswhere.exe -nologo -products * -version [17.0,18.0] -property installationPath`) do (
+  set "VSINSTALLDIR=%%i\\"
+)
+if not exist "%VSINSTALLDIR%" (
+  echo "Could not find VS 2022"
+  exit /B 1
+)
+
+echo "Found VS 2022 in %VSINSTALLDIR%"
+call "%VSINSTALLDIR%VC\\Auxiliary\\Build\\vcvarsall.bat" x64
+
 mkdir build
 cd build
 
