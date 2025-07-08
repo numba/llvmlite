926081ce46dc5bc18b055544619778a63dd09684
swap357 <swapnilpatel357@gmail.com>
2025-07-08 14:26:31 -0700

    replace 'linux-arm64' with 'linux-aarch64' to match conda standards

--- buildscripts/github/llvmdev_evaluate.py
+++ buildscripts/github/llvmdev_evaluate.py
@@ -11,7 +11,7 @@ inputs = os.environ.get("GITHUB_WORKFLOW_INPUT", "{}")
 
 runner_mapping = {
     "linux-64": "ubuntu-24.04",
-    "linux-arm64": "ubuntu-24.04-arm",
+    "linux-aarch64": "ubuntu-24.04-arm",
     "osx-64": "macos-13",
     "osx-arm64": "macos-14",
     "win-64": "windows-2025",
@@ -29,15 +29,15 @@ default_include = [
         "platform": "linux-64",
         "recipe": "llvmdev_for_wheel",
     },
-    # linux-arm64
+    # linux-aarch64
     {
-        "runner": runner_mapping["linux-arm64"],
-        "platform": "linux-arm64",
+        "runner": runner_mapping["linux-aarch64"],
+        "platform": "linux-aarch64",
         "recipe": "llvmdev",
     },
     {
-        "runner": runner_mapping["linux-arm64"],
-        "platform": "linux-arm64",
+        "runner": runner_mapping["linux-aarch64"],
+        "platform": "linux-aarch64",
         "recipe": "llvmdev_for_wheel",
     },
     # osx-64
