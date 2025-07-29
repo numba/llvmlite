a5e9cbf997b027be9b3a7deb4368f7f8768c8e1f
swap357 <swapnilpatel357@gmail.com>
2025-07-28 21:54:41 -0700

    add env variable to auto-accept miniconda tos during installation

--- buildscripts/manylinux/prepare_miniconda.sh
+++ buildscripts/manylinux/prepare_miniconda.sh
@@ -5,5 +5,6 @@ curl -L -o mini3.sh $1
 bash mini3.sh -b -f -p /root/miniconda3
 echo "Miniconda installed"
 source /root/miniconda3/bin/activate base
+export CONDA_PLUGINS_AUTO_ACCEPT_TOS=1
 echo "Env activated"
 cd -
