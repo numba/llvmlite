696b1a7e3e50897a1d89589e73523b9cee49873a
Stuart Archibald <stuartarchibald@users.noreply.github.com>
2025-08-15 13:26:19 +0100

    Add back in bootstrapping on OSX incremental build
    As title. This probably only works on conda build as it
    provides the necessary isolation.

--- buildscripts/incremental/build.sh
+++ buildscripts/incremental/build.sh
@@ -4,12 +4,17 @@ set -x
 
 source activate $CONDA_ENV
 
-# Set up osx.
+# need to build with Anaconda compilers on osx, but they conflict with
+# llvmdev... bootstrap
 if [[ $(uname) == Darwin ]]; then
-  if [ -n "$MACOSX_DEPLOYMENT_TARGET" ]; then
-    export MACOSX_DEPLOYMENT_TARGET=10.15
-  fi
-  export DARWIN_TARGET=x86_64-apple-darwin13.4.0
+  # Set conda subdir and bootstrap with x86_64 compiler
+  CONDA_SUBDIR=osx-64
+  conda create -y -p ${PWD}/bootstrap clangxx_osx-64
+
+  SRC_DIR=${PWD}
+  export PATH=${SRC_DIR}/bootstrap/bin:${PATH}
+  CONDA_PREFIX=${SRC_DIR}/bootstrap \
+    . ${SRC_DIR}/bootstrap/etc/conda/activate.d/*
 
   # Use explicit SDK path if set, otherwise detect
   if [ -z "$SDKROOT" ]; then
@@ -18,9 +23,23 @@ if [[ $(uname) == Darwin ]]; then
     SDKPATH=$SDKROOT
   fi
   export CONDA_BUILD_SYSROOT=${CONDA_BUILD_SYSROOT:-${SDKPATH}}
+
+  # Set minimum deployment target if not already set
+  if [ -z "$MACOSX_DEPLOYMENT_TARGET" ]; then
+    export MACOSX_DEPLOYMENT_TARGET=11.0
+  fi
+
+  export CXXFLAGS=${CFLAGS}" -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
+  export CFLAGS=${CFLAGS}" -mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET}"
   SYSROOT_DIR=${CONDA_BUILD_SYSROOT}
   CFLAG_SYSROOT="--sysroot ${SYSROOT_DIR}"
   export SDKROOT=${SDKPATH}
+
+  DARWIN_TARGET=x86_64-apple-darwin13.4.0
+fi
+
+if [ -n "$MACOSX_DEPLOYMENT_TARGET" ]; then
+    export MACOSX_DEPLOYMENT_TARGET
 fi
 
 export PYTHONNOUSERSITE=1
