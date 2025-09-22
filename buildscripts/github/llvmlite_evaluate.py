0a6c7c1354f92dd633f2a3fd5705373d8c3e96f0
swap357 <swapnilpatel357@gmail.com>
2025-09-22 09:33:29 -0700

    add python3.14 to CI matrix and add EXTRA_CHANNELS environment variable to conda builder

--- buildscripts/github/llvmlite_evaluate.py
+++ buildscripts/github/llvmlite_evaluate.py
@@ -22,24 +22,28 @@ default_include = [
     {"runner": runner_mapping["linux-64"], "platform": "linux-64", "python-version": "3.11"},
     {"runner": runner_mapping["linux-64"], "platform": "linux-64", "python-version": "3.12"},
     {"runner": runner_mapping["linux-64"], "platform": "linux-64", "python-version": "3.13"},
+    {"runner": runner_mapping["linux-64"], "platform": "linux-64", "python-version": "3.14"},
 
     # linux-aarch64
     {"runner": runner_mapping["linux-aarch64"], "platform": "linux-aarch64", "python-version": "3.10"},
     {"runner": runner_mapping["linux-aarch64"], "platform": "linux-aarch64", "python-version": "3.11"},
     {"runner": runner_mapping["linux-aarch64"], "platform": "linux-aarch64", "python-version": "3.12"},
     {"runner": runner_mapping["linux-aarch64"], "platform": "linux-aarch64", "python-version": "3.13"},
+    {"runner": runner_mapping["linux-aarch64"], "platform": "linux-aarch64", "python-version": "3.14"},
 
     # osx-arm64
     {"runner": runner_mapping["osx-arm64"], "platform": "osx-arm64", "python-version": "3.10"},
     {"runner": runner_mapping["osx-arm64"], "platform": "osx-arm64", "python-version": "3.11"},
     {"runner": runner_mapping["osx-arm64"], "platform": "osx-arm64", "python-version": "3.12"},
     {"runner": runner_mapping["osx-arm64"], "platform": "osx-arm64", "python-version": "3.13"},
+    {"runner": runner_mapping["osx-arm64"], "platform": "osx-arm64", "python-version": "3.14"},
 
     # win-64
     {"runner": runner_mapping["win-64"], "platform": "win-64", "python-version": "3.10"},
     {"runner": runner_mapping["win-64"], "platform": "win-64", "python-version": "3.11"},
     {"runner": runner_mapping["win-64"], "platform": "win-64", "python-version": "3.12"},
     {"runner": runner_mapping["win-64"], "platform": "win-64", "python-version": "3.13"},
+    {"runner": runner_mapping["win-64"], "platform": "win-64", "python-version": "3.14"},
 ]
 
 print(
